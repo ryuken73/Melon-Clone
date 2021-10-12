5c0d2c0b70f4d894537836c9109a9d964204937b
ryuken73 <ryuken73@gmail.com>
2021-10-12 23:37:03 +0900

    update: make hoverbutton's opacity lower when disabled

--- src/Components/Common/ButtonHover.js
+++ src/Components/Common/ButtonHover.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 import IconButton from '@mui/material/IconButton';
 
-
 const HoverButton = styled(IconButton)`
+    &.Mui-disabled {
+        opacity: ${props => props.opacityDisabled || '0.3'};
+    }
     .MuiSvgIcon-root {
         color: ${props => props.color || 'white'};
         opacity: ${props => props.opacitynormal || '0.5'};
