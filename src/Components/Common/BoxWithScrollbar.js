8bec47f427df7991154f614070ca8c5897f88014
ryuken73 <ryuken73@gmail.com>
2021-10-13 23:02:26 +0900

    UI: add new component(SpanBox and BoxWithScrollbar)

---
+++ src/Components/Common/BoxWithScrollbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import styled from 'styled-components';
+import ScrollBarWithColor from '../Common/ScrollBarWithColor';
+
+const Container = styled(Box)`
+    && {
+        display: flex;
+        flex-direction: column;
+        color: white;
+        background: transparent;
+        overflow:hidden;
+    }
+`
+
+const BoxWithScrollbar = props => {
+    console.log(props)
+    return (
+        <ScrollBarWithColor autoHide style={{width:'auto', height: '100%' }}>
+            <Container>
+                {props.children}
+            </Container>
+        </ScrollBarWithColor>
+    )
+}
+
+export default React.memo(BoxWithScrollbar);
---
+++ src/Components/Common/SpanBox.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Span = styled.span`
+    font-size: ${props => props.fontSize || "12px"};
+    color: ${props => props.color || "darkgrey"};
+    font-weight: ${props => props.fontWeight || "bold"};
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+`
+
+const SpanBox = props => {
+    const {
+        onClick=()=>{},
+        text="Text"
+    } = props;
+    return (
+            <Span
+                onClick={onClick}
+                {...props}
+            >
+                {text}
+            </Span>
+    )
+}
+
+export default SpanBox;
