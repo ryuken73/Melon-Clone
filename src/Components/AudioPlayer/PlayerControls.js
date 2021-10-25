be7ca7fd4c65961a566b5b57473996a5af7aa957
ryuken73 <ryuken73@gmail.com>
2021-10-26 01:10:21 +0900

    update: making audio player with hls.js (this time ugly!)

--- src/Components/AudioPlayer/PlayerControls.js
+++ src/Components/AudioPlayer/PlayerControls.js
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import styled from 'styled-components';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import Replay10Icon from '@mui/icons-material/Replay10';
@@ -20,16 +21,43 @@ const Container = styled(Box)`
     justify-content: center;
     background: black;
 `
+
+const isPlaying = player => !!(player !== null && player.currentTime > 0 && !player.paused && !player.ended && player.readyState > 2);
+
 const PlayerControls = props => {
+    const {
+        playerRef=null, 
+        canPlay=false, 
+        currentEvent=null
+        // playerStopped=true
+    } = props;
+
     const {
         onClickRepeat=()=>{},
         onClickReplay10=()=>{},
         onClickSkipPrevious=()=>{},
-        onClickPlay=()=>{},
         onClickSkipNext=()=>{},
         onClickForward10=()=>{},
-        onClickVolumeUp=()=>{}
-    } = props;
+        onClickVolumeUp=()=>{},
+        // onClickPlay=()=>{},
+    } = props;   
+
+    const player = playerRef.current;
+
+    const [isPlaying, setIsPlaying] = React.useState(false);
+    React.useEffect(() => {
+        console.log('currentEvent:', currentEvent)
+        setIsPlaying(!!(player !== null && player.currentTime > 0 && !player.paused && !player.ended && player.readyState > 2))
+    },[currentEvent, player])
+
+    const onClickPlay = React.useCallback(() => {
+        if(playerRef === null) return;
+        if(isPlaying){
+            player.pause();
+            return
+        }
+        player.play();
+    },[player,isPlaying, playerRef])
 
     const [volumeIconActive, setVolumeIconActive] = React.useState(false);
 
@@ -49,7 +77,12 @@ const PlayerControls = props => {
             <HoverButton onClick={onClickRepeat}><RepeatIcon fontSize="small"></RepeatIcon></HoverButton>
             <HoverButton onClick={onClickReplay10}><Replay10Icon fontSize="small"></Replay10Icon></HoverButton>
             <HoverButton onClick={onClickSkipPrevious}><SkipPreviousIcon></SkipPreviousIcon></HoverButton>
-            <HoverButton onClick={onClickPlay} opacitynormal='0.7' opacityhover='1'><PlayArrowIcon fontSize="large" ></PlayArrowIcon></HoverButton>
+            <HoverButton onClick={onClickPlay} opacitynormal='0.7' opacityhover='1' disabled={!canPlay}>
+                {isPlaying ?                     
+                    <PauseIcon fontSize="large" ></PauseIcon> :
+                    <PlayArrowIcon fontSize="large" ></PlayArrowIcon>                  
+                }
+            </HoverButton>
             <HoverButton onClick={onClickSkipNext}><SkipNextIcon></SkipNextIcon></HoverButton>
             <HoverButton onClick={onClickForward10}><Forward10Icon fontSize="small"></Forward10Icon></HoverButton>
             <HoverButton onClick={onClickVolumeControl} opacitynormal={volumeIconActive ? 1 : 0.7} ref={anchorElRef}><VolumeUpIcon></VolumeUpIcon></HoverButton>
--- src/Components/AudioPlayer/index.js
+++ src/Components/AudioPlayer/index.js
@@ -8,6 +8,7 @@ import SliderBar from '../Common/SliderBar';
 import PlayerControls from './PlayerControls';
 import colors from '../../config/colors';
 import { ColorizeSharp } from '@mui/icons-material';
+import Hls from 'hls.js';
 
 const Container = styled(Box)`
     display: flex;
@@ -40,7 +41,63 @@ const Controls = styled(Box)`
     margin-top: auto;
 `;
 
-const AudioPlayer = () => {
+
+
+const eventHandlers = {
+    'abort': 'onAbort',
+    'canplay': 'onCanPlay',
+    'canplaythrough': 'onCanPlayThrough',
+    'durationchange': 'onDurationChange',
+    'progress': 'onProgress',
+    'timeupdate': 'onTimeUpdate',
+    'pause': 'onPause',
+    'play': 'onPlay',
+    'seeked': 'onSeeked',
+    'error': 'onError',
+    'ended': 'onEnded'
+
+}
+
+const hlsSource = 'http://10.11.31.51:1935/music/_definst_/mp3:Audio_WAVE1/4MB/4MB029154/4MB029154_Track01.mp3/playlist.m3u8'
+
+const AudioPlayer = props => {
+    const [hls, setHls] = React.useState(null);
+    const [canPlay, setCanPlay] = React.useState(false);
+    const [currentEvent, setCurrentEvent] = React.useState(null);
+
+    const playerDefaultEventHandler = React.useCallback(event => {
+        console.log(`event on:`,event.type, event.target.src)
+        setCurrentEvent(event.type)
+    },[]);
+
+    let playerRef = React.useRef(null);
+
+    React.useEffect(()=>{
+        if(Hls.isSupported()){
+            const audioElement = new Audio();
+            playerRef.current = audioElement;
+            const hls = new Hls();
+            hls.attachMedia(audioElement);
+            hls.on(Hls.Events.MEDIA_ATTACHED, () => {
+                console.log('audio is attached to hls');
+                playerRef.current = audioElement;
+                setHls(hls);
+            })
+            Object.keys(eventHandlers).forEach(eventType => {
+                audioElement.addEventListener(eventType, props[eventHandlers[eventType]]||playerDefaultEventHandler)
+            })
+        }
+    },[]);
+
+    React.useEffect(()=>{
+        if(hls === null) return;
+        hls.loadSource(hlsSource);
+        hls.on(Hls.Events.MANIFEST_PARSED, (event, data) => {
+            console.log('manifest loaded, found data:', data);
+            setCanPlay(true)
+        })
+    },[hls])
+
     return (
         <Container>
             <Image>
@@ -60,7 +117,7 @@ const AudioPlayer = () => {
                 <TextBox fontSize="11px" text="03:01" marginLeft="5px" color={colors.textSub}></TextBox>
             </Duration>
             <Controls>
-                <PlayerControls></PlayerControls>
+                <PlayerControls playerRef={playerRef} canPlay={canPlay} currentEvent={currentEvent}></PlayerControls>
             </Controls>
         </Container>
     )
