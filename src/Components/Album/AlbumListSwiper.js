65ea78a98f516e89c817b057f7c32af8ec85aa05
ryuken73 <ryuken73@gmail.com>
2021-11-02 00:32:16 +0900

    update: get recent albums in swipe view using custom hook fetch

--- src/Components/Album/AlbumListSwiper.js
+++ src/Components/Album/AlbumListSwiper.js
@@ -3,7 +3,8 @@ import Box from '@mui/material/Box';
 import styled from 'styled-components';
 import AlbumBox from './AlbumBox';
 import Swiper from '../Common/Swiper';
-import useFetchRecentAlbums from '../../hooks/useFetchRecentAlbums';
+import useFetchAlbums from '../../hooks/useFetchAlbums';
+import {getString} from '../../lib/util';
 
 const Container = styled(Box)`
     display: flex;
@@ -11,11 +12,30 @@ const Container = styled(Box)`
     width: ${prop => prop.height || "auto"};
     margin-top: 10px;
 `
+const PAGE_NUM = 1;
 const FETCH_COUNT = 15;
+
+const getDateTimeString = () => {
+    const now = new Date();
+    return getString(now, {sep:''}).substring(0,12);
+}
 
 const AlbumList = props => {
     const {history} = props;
-    const {albums, error, loading} = useFetchRecentAlbums(FETCH_COUNT);
+    const [options, setOptions] = React.useState({});
+    React.useEffect(()=>{
+        const fetchOptions = {
+            'page_num': PAGE_NUM,
+            'page_sizes': FETCH_COUNT,
+            'scn': 'album',
+            'query': `status='Y' and open_time <= '${getDateTimeString()}'`,
+            'orderby': "order by open_dt desc",
+            'bool': true            
+        }
+        setOptions(fetchOptions);
+    },[])
+
+    const {albums, error, loading} = useFetchAlbums(options);
     return (
         <Container width="100%">
             {albums.length > 0 &&
--- src/hooks/useFetchAlbums.js
+++ src/hooks/useFetchAlbums.js
@@ -1,60 +1,43 @@
 import React from 'react';
 import useFetch from 'use-http';
 import {baseUrl, apiMap, responseToObject} from '../config/apis';
-import {getString} from '../lib/util';
-import usePrevious from './usePrevious';
-
 
 const SERVER_NAME = 'musicbank';
 const API_NAME = 'searchAlbum';
 
-export default function useFetchAlbums(pathname, pageNum, count) {
-    const [albums, setAlbums] = React.useState([]);
-    const api = apiMap[API_NAME];
-    const {uri, headers} = api;
-    const fetchOptions = {
-        headers: {
-            'Content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        }
+const api = apiMap[API_NAME];
+const {uri, headers} = api;
+const fetchOptions = {
+    headers: {
+        'Content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
     }
-    const genre = {
-        'kpop': 4,
-        'pop': 58,
-        'classic': 144,
-        'etc': 0
-    }
+}
+
+export default function useFetchAlbums(options, newFetchRequired=true) {
+    const [albums, setAlbumsFetched] = React.useState([]);
     const {post, response, error, loading} = useFetch(fetchOptions);
-    const previousPageNum = usePrevious(pageNum);
-    React.useEffect(() => {
-        setAlbums([]);
-    },[pathname])
-
-    React.useEffect(()=>{
-        async function fetchData(pathname, pageNum, count){
-            const now = new Date();
-            const currentDateTimeString = getString(now, {sep:''}).substring(0,12);
-            const genreParam = pathname === 'all' ? '' : `and top_genre=${genre[pathname]}`
-            const searchParam = new URLSearchParams();
-            searchParam.append('page_num', pageNum);
-            searchParam.append('page_sizes', count);
-            searchParam.append('scn', 'album');
-            searchParam.append('query', `status='Y' and open_time <= '${currentDateTimeString}' ${genreParam}`);
-            searchParam.append('orderby', "order by open_dt desc");
-            searchParam.append('bool', "true");
-            const newAlbums = await post(uri, searchParam);
-            if(response.ok && newAlbums.cols !== undefined){
-                const {fdata} = newAlbums;
-                const albums = responseToObject(fdata, headers);
-                const imagePathAttachedAlbums = albums.map(album => {
-                    return {...album, eval_imagePath: `${baseUrl[SERVER_NAME]}/Video/small_image/${album.label_no}.JPG`}
-                })
-                setAlbums(albums => [...albums, ...imagePathAttachedAlbums])
-            }
+    const fetchData = React.useCallback(async () => {
+        const searchParam = new URLSearchParams();
+        Object.keys(options).forEach(optionKey => {
+            searchParam.append(optionKey, options[optionKey])
+        })
+        const newAlbums = await post(uri, searchParam);
+        if(response.ok && newAlbums.cols !== undefined){
+            const {fdata} = newAlbums;
+            const albums = responseToObject(fdata, headers);
+            const imagePathAttachedAlbums = albums.map(album => {
+                return {...album, eval_imagePath: `${baseUrl[SERVER_NAME]}/Video/small_image/${album.label_no}.JPG`}
+            })
+            setAlbumsFetched(imagePathAttachedAlbums)
         }
-        console.log(pathname, pageNum, previousPageNum, count)
-        if(pageNum !== 1 && pageNum === previousPageNum) return ;
-        fetchData(pathname, pageNum, count);
-    },[pathname, pageNum, count]);
-
-    return {albums, error, loading};
+    },[options])
+    React.useEffect(()=>{
+        console.log('useFetchAlbum:', options, newFetchRequired)
+        if(newFetchRequired){
+            fetchData();
+        } else {
+            setAlbumsFetched([]);
+        }
+    },[options, newFetchRequired]);
+    return {albums, error, loading, fetchData};
 }
