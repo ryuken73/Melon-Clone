002a89b58cb121bff9463f6605806352c458245b
ryuken73 <ryuken73@gmail.com>
2021-11-03 11:45:38 +0900

    update: apply redux-logger

--- src/store.js
+++ src/store.js
@@ -1,8 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { getDefaultNormalizer } from "@testing-library/dom";
+import { createLogger } from 'redux-logger';
 import albumReducer from './Components/Album/albumSlice'
+
+const logger = createLogger();
 
 export const store = configureStore({
     reducer: {
         album: albumReducer,
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    devTools: process.env.NODE_ENV !== 'production'
 })
