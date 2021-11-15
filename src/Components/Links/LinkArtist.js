34034f27d579c16cc96e7ba655239c3a620ffffb
ryuken <ryuken73@gmail.com>
2021-11-15 20:22:39 +0900

    update: if artist is not registered show alert message(todo: need common alert dialog)

--- src/Components/Links/LinkArtist.js
+++ src/Components/Links/LinkArtist.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import TextBox from 'Components/Common/TextBox';
 import {withRouter} from 'react-router-dom';
+import useArtistId from 'hooks/useArtistId';
 
 const LinkArtist = props => {
     const {artist} = props;
     const {history} = props;
+    const [hasArtistId, setHasArtistId] = React.useState(false);
+    const {isSuccess, data} = useArtistId(artist);
+    React.useEffect(() => {
+        data?.list?.length > 0 && setHasArtistId(true);
+    },data)
     const onClick = React.useCallback(event=>{
+        if(!hasArtistId){
+            alert('artist not found');
+            return;
+        }
         const artist = event.target.innerText;
         history.push(`/artist/${artist}/songList`)
-    },[history])
+    },[history, hasArtistId])
     return (
         <TextBox text={artist} onClick={onClick}></TextBox>
     )
