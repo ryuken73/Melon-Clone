587586c1d651513821b964f4fa2c231773e8f10a
ryuken73 <ryuken73@gmail.com>
2021-11-27 23:25:47 +0900

    fix: update not found default image of image box

--- src/Components/Common/ImageBox.js
+++ src/Components/Common/ImageBox.js
@@ -37,7 +37,7 @@ const THRESHOLD = CONSTANTS.IMAGE_LAZY_SHOW_THRESHOLD
 
 const ImageBox = props => {
     const {
-        src='/images/4MB029205.jpg',
+        src='/images/no_image_black.jpg',
         onClick=()=>{},
         alt="image",
         title="related image",
