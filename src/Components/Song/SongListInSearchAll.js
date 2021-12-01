28e8930316fd8e5ee8835a35551feaf06ace5a56
ryuken73 <ryuken73@gmail.com>
2021-12-01 10:49:09 +0900

    refactor: remove unused and change key of child element

--- src/Components/Song/SongListInSearchAll.js
+++ src/Components/Song/SongListInSearchAll.js
@@ -3,7 +3,6 @@ import Box from '@mui/material/Box';
 import styled from 'styled-components';
 import SongIteminSearchAll from './SongIteminSearchAll';
 import Divider from '../Common/Divider';
-import ScrollBarWithColor from '../Common/ScrollBarWithColor';
 
 const Container = styled(Box)`
     && {
@@ -14,37 +13,21 @@ const Container = styled(Box)`
 
 const SongListInSearchAll = props => {
     const {songs} = props;
-    const initialCheckedArray = new Array(songs.length);
-    initialCheckedArray.fill(false);
-    const [checkedArray, setCheckedArray] = React.useState(initialCheckedArray);
     const {mr="15px"} = props;
-
-    const toggleChecked = React.useCallback(index => {
-        setCheckedArray(checkedArray => {
-            const newChecked = !checkedArray[index];
-            const newCheckedArray = [...checkedArray]
-            newCheckedArray[index] = newChecked;
-            return newCheckedArray;
-        })
-    },[setCheckedArray])
     return (
         <Container>
-            {/* <ScrollBarWithColor autoHide style={{ width:'100%', height: 'calc(100vh - 370px)' }}> */}
-                {songs.map((song, index) => (
-                    <Box key={song.rownum} px="10px">
-                        <SongIteminSearchAll
-                            rownum={index}
-                            fontSize="14px"
-                            color="white"
-                            song={song}
-                            checked={checkedArray[index]}
-                            width="100%"
-                            toggleChecked={toggleChecked}
-                        ></SongIteminSearchAll>
-                        <Divider opacity="0.2" margin="0px" mr={mr}></Divider>
-                    </Box>
-                ))}
-            {/* </ScrollBarWithColor> */}
+            {songs.map((song, index) => (
+                <Box key={song.id} px="10px">
+                    <SongIteminSearchAll
+                        rownum={index}
+                        fontSize="14px"
+                        color="white"
+                        song={song}
+                        width="100%"
+                    ></SongIteminSearchAll>
+                    <Divider opacity="0.2" margin="0px" mr={mr}></Divider>
+                </Box>
+            ))}
         </Container>
     )
 }
