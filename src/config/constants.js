ba39d6de48254940b4f2819f1de2e2b9993583ed
ryuken73 <ryuken73@gmail.com>
2022-01-10 23:43:51 +0900

    update: change bora url from http to https (media.capturestream only works https in different origin condition

--- src/config/constants.js
+++ src/config/constants.js
@@ -5,7 +5,7 @@ const dev = {
     "DOWNLOAD_URL": 'http://10.11.31.51:8554/music',
     "DOWNLOAD_URL_ONAIR": 'http://10.11.31.51:8554/onair',
     "AUTOCOMPLETE_URL": 'http://10.11.31.51:3010',
-    "BASE_BORA_STREAM_URL": 'http://cmsrelay.sbs.co.kr',
+    "BASE_BORA_STREAM_URL": 'https://cmsrelay.sbs.co.kr',
     "DEFAULT_PROGRAM_ATTACH_PATH": '/Video/pgm_img/',
     "GET_MORE_WAIT_SEC_DEBOUNCE": 100,
     "ALBUM_PAGE_SIZE": 20,
