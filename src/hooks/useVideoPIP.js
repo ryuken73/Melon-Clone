08f2711d68a825892afb331c33004190786856bd
ryuken73 <ryuken73@gmail.com>
2022-01-13 00:32:24 +0900

    fix: apply try catch to pip handling (makes many error)

--- src/hooks/useVideoPIP.js
+++ src/hooks/useVideoPIP.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-import useMediaQueryEasy from './useMediaQueryEasy';
+import useMediaQueryEasy from 'hooks/useMediaQueryEasy';
+import useMessageBox from 'hooks/useMessageBox'
 import CONSTANTS from 'config/constants';
 
 const {SRC_TYPE} = CONSTANTS;
@@ -8,12 +9,17 @@ const {SRC_TYPE} = CONSTANTS;
 export default function usePIP() {
     const srcType = useSelector(state => state.audioPlayer.currentSrcType);
     const {hideRightPane} = useMediaQueryEasy();
+    const {showMessageBox} = useMessageBox();
     const showPIP = React.useCallback( mediaElementRef => {
         // if(mediaElementRef.current === null) return;
         // if(mediaElementRef.current.readyState && mediaElementRef.current.readyState === 0) return;
         if(srcType === SRC_TYPE.BORA && hideRightPane){
             if(!document.pictureInPictureElement){
-                mediaElementRef.current.requestPictureInPicture();
+                mediaElementRef.current.requestPictureInPicture()
+                .catch(err => {
+                    console.error(err);
+                    showMessageBox('PIP Player Loading Error(temporary)', 1000, 'error')
+                })
             }
         }
     }, [srcType, hideRightPane])
