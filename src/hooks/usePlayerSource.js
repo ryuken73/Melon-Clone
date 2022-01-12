2de53fd82303d373577cf5ef1b2377eeee9e498c
ryuken73 <ryuken73@gmail.com>
2022-01-13 00:33:42 +0900

    todo: periodically reload Portal, shows very short previous shot in pip mode

--- src/hooks/usePlayerSource.js
+++ src/hooks/usePlayerSource.js
@@ -64,6 +64,7 @@ export default function usePlayer(src, mediaElementRef, src_type) {
 
         if(src_type === SRC_TYPE.POT_CAST || src_type === SRC_TYPE.BORA) {
             console.log('!! attach loadedmetadata event handler to media element(not hls) and set media source');
+            mediaElementRef.current.src = null;
             mediaElementRef.current.addEventListener('loadedmetadata', handleLoadedMetadata)
             mediaElementRef.current.src = src;
             dispatch(setManifestLoaded({manifestLoaded:true}));
