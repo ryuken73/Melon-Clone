045f2b98f7188847e0d86d72445eadfb145434cf
ryuken73 <ryuken73@gmail.com>
2022-01-23 11:28:39 +0900

    UI: adjust position of drag handle

--- src/Components/PlayList/Song.js
+++ src/Components/PlayList/Song.js
@@ -115,7 +115,7 @@ const Song = props => {
                 <TextBox clickable text={song_name} onDoubleClick={onDoubleClick} doubleClicked={doubleClicked} color={currentPlaying && 'gold'} margin="0px 15px 0px 0px" width="125px"></TextBox>
             </Artist>
             <TextBox text={artist} color={currentPlaying && 'gold'} width="90px"></TextBox>
-            <Box width="30px" {...provided.dragHandleProps}>
+            <Box width="30px" pr="10px" {...provided.dragHandleProps}>
                 <CustomHandleIcon></CustomHandleIcon>
             </Box>
         </Container>
